3c426524347c12e10586b8b18fa0fa707570e02c
Your Name <you@example.com>
2019-09-18 15:11:34 +0000

    init some components

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+import Header from './components/header/header';
+
+export default class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Hello, React!</h1>
+				<Header />
+				<div>yy yyy</div>
 			</div>
 		)
 	}
 }
 
-export default App;
