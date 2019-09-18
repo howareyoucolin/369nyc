b041a7914e23d8ce83be63c218a1ed254f429065
Your Name <you@example.com>
2019-09-18 21:03:56 +0000

    re-structure component folder

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Header from './components/header/header';
+import Header from './components/header';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
 				<Header />
-				<div>yy yyy</div>
+				<div>yy yyy a</div>
 			</div>
 		)
 	}
