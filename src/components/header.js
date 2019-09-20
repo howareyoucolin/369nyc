f169737f710ce3141c0b228589b95750fba063a1
Your Name <you@example.com>
2019-09-20 23:20:15 +0000

    withStyles takes 2 params

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import withStyles from 'isomorphic-style-loader/withStyles';
+import gs from './global.css';
 import s from './header.css';
 
 class Header extends React.Component {
 	render() {
 		return (
 			<div className={s.header}>
-				<h1 className={s.title}>369纽约活动网</h1>
+				<div className={gs.container}>
+					<h1 className={s.title}>369纽约活动网</h1>
+				</div>
 			</div>
 		)
 	}
 }
 
-export default withStyles(s)(Header);
+export default withStyles(s,gs)(Header);
