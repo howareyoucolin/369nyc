6a16a9b22104473af76a422053b33901366ad451
Your Name <you@example.com>
2019-09-22 21:24:25 +0000

    setup async/await

--- src/components/events/events.js
+++ src/components/events/events.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import withStyles from 'isomorphic-style-loader/withStyles';
+import axios from 'axios';
 import gs from 'src/global.css';
 
 class Events extends React.Component {
+	
+	constructor(props) {
+		super(props);
+	}
+	
+	async _getData() {
+		let results = await axios.get('http://api.369usa.com/posts');
+		return results.data;
+	}
+	
 	render() {
 		return (
 			<div>
-				This is a list of Events.
+				<p>This is a list of Events.</p>
+				<ul>
+					{posts.map(post => <li>{post.post_title}</li>)}
+				</ul>
 			</div>
 		)
 	}
