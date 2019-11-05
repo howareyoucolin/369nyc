511906830ecbb7398f73443dc82e9541d6aa6eca
Your Name <you@example.com>
2019-11-05 04:10:06 +0000

    fix conditional rendering glitch

--- src/components/members/members.js
+++ src/components/members/members.js
@@ -25,7 +25,7 @@ class Members extends React.Component {
 							<span className={s.male}>&#9794;</span>
 						</div>
 						<p className={s.subtitle}>法拉盛, 纽约</p>
-						{member.wechat !== null &&
+						{member.wechat != false &&
 							<p>微信号: {member.wechat}</p>
 						}
 						<p>{member.content}</p>
